668817b92cb4e08fa71784a9714766b800f64592
Wander Hillen <wjw.hillen@gmail.com>
2016-08-19 22:15:04 +0200

    Reworked nivix.hs, changed datastore to SQLite and ripped out message bus idea.

--- nivix.hs
+++ nivix.hs
@@ -1,114 +1,60 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Control.Concurrent
-import Control.Monad
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 import Control.Monad.IO.Class
 import Data.Aeson
-import Data.IORef
-import Data.Sequence
-import Network.HTTP.Types.Status
-import System.Time
+import Database.SQLite.Simple
+import Data.Time
+import GHC.Generics
+import Network.HTTP.Types
 import Web.Scotty
--- import qualified Data.ByteString.Lazy.Char8 as BSL
 
-import Nivix.Types
+--simple IOT server for the sensors in the watermill
+--current functions: 
+-- receive posts from the sensor and persist them to a sqlite db
+-- show the contents of the db in a JSON format
+-- serve a webpage that will parse the JSON from the last line into a nice looking graph
 
--- Nivix is the home of the firemind, a place where all crazy experiments live
--- Nivix zal vier taken hebben:
--- 1. Dashboard server.
--- 2. Dead mans snitch functionaliteit
--- 3. Stayalives teruggeven wanneer dat nodig is. 
--- 4. Alle events persisten naar disk.
-
-mainlog = "nivix-log.txt"
-keepAliveFile = "keepalive.txt"
+dbpath = "nivixdb.sqlite"
 
 main = do
-    firemind <- newChan :: IO (Chan Event)   --main event channel, hiervan gaat iedereen lezen of een dupchan maken
-    forkIO $ persister firemind         --persister schrijft alle items veilig weg naar disk
-    seqref <- newIORef empty
-    (dupChan firemind) >>= forkIO . dashboardBufferer seqref
-    nu <- getClockTime
-    snitchLastTime <- newIORef nu -- :: IO (IORef ClockTime)
-    snitchChan <- (dupChan firemind)
-    forkIO $ snitch snitchLastTime snitchChan
-    scotty 3000 $ do
+    conn <- open dbpath
+    scotty 80 $ do
         get  "/" $ showDashboard --serve een static file met de homepage
-        get  "/history" $ getHistory --seqref
-        -- get  "/snitch" $ getSnitch snitchLastTime
-        post "/" $ receiveEvent firemind --een nieuwe event
-        notFound show404                        -- deze matcht alles, net als "otherwise" in een guard
+        get  "/history" $ getHistory conn
+        post "/" $ receiveEvent conn --een nieuwe event
+        notFound show404 -- deze matcht alles, net als "otherwise" in een guard
 
 --serve homepage/dashboard
 showDashboard :: ActionM () --ActionM () is de type signature van een route handler, het is een monad waarin je de response kan samenstellen
 showDashboard = do
-    setHeader "Content-Type" "text/plain" --sets the headers, in dit geval de Content-Type header met value text/plain
-    file "scottytest.hs" --body van de response is een file (scottytest.hs dus)
+    setHeader "Content-Type" "text/html" --sets the headers, in dit geval de Content-Type header met value text/plain
+    file "homepage.html" --body van de response is een file (scottytest.hs dus)
         
 --ontvang een POST request, als het een geldig event is, schrijf het weg naar de 
-receiveEvent :: Chan Event -> ActionM ()
-receiveEvent firemind = do
-    now <- liftIO getClockTime
-    liftIO $ print $ "received event at " ++ (show now)
+receiveEvent :: Connection -> ActionM ()
+receiveEvent conn = do
     bdy <- body
     event <- (return $ decode bdy :: ActionM (Maybe Event))
-    case event of
-        Nothing -> show404
-        Just (STUW si sv) -> (liftIO getClockTime) >>= (\(TOD x _) -> liftIO (writeChan firemind (STUWWITHTIME x si sv))) --voegt tijd toe aan de event, want de Pi heeft geen real time clock
-        Just evt -> liftIO (writeChan firemind evt)
+    case (decode bdy) of
+        Nothing -> return ()
+        Just (Stuw batlevel temp) -> liftIO $ do
+            now <- getCurrentTime
+            execute conn "INSERT INTO Measurements VALUES (?,?,?)" (now,batlevel,temp)
     setHeader "Content-Type" "text/plain"
-    shouldStayAlive <- liftIO $ readFile keepAliveFile
-    case shouldStayAlive of
-        "YES"   -> text "stayalive"
-        "NO"    -> text "NO"
-        _       -> text "NO"
-    
+    text "NO" --could also be "keepalive", but that's for later
 
---schrijft alle events naar het main logfile
-persister :: Chan Event -> IO ()
-persister firemind = forever $ do
-    currentEvent <- readChan firemind
-    now <- getClockTime
-    (appendFile mainlog) $ concat [(show now)," ",(show currentEvent), "\n"]
-    print $ currentEvent
+--dit is eigenlijk een Stuw batlevel temp, maar is gedefinierd zonder namen voor de types omdat 
+--de logger op de RPi op een versie van GHC werkt die geen Template Haskell ondersteunt vanwege
+--dat de Pi een ARM processor heeft. TH is nodig om Aeson te draaien. Dit is een goed compromis
+--omdat de JSON encoding van deze ook makkelijk handmatig te genereren is op de Pi.
+data Event = Stuw Double Double deriving (Show,Generic)
+instance ToJSON Event
+instance FromJSON Event
 
 --
-dashboardBufferer :: IORef (Seq Event) -> Chan Event -> IO ()
-dashboardBufferer seqref chan = forever $ do
-    currentEvent <- readChan chan
-    now <- getClockTime
-    appendToBuffer seqref currentEvent
-
--- 
-appendToFixedSizeSequence :: Int -> Event -> Seq Event -> Seq Event
-appendToFixedSizeSequence maxsize newEvent startSeq = if (intermediateLength > maxsize) then newseq else intermediateSeq
-    where   intermediateSeq = startSeq |> newEvent
-            intermediateLength = Data.Sequence.length intermediateSeq
-            (_ :< newseq) = viewl intermediateSeq
-            
-appendToBuffer :: IORef (Seq Event) -> Event -> IO ()
-appendToBuffer ref evt = modifyIORef' ref $ appendToFixedSizeSequence 200 evt
-
---deze draait als aparte thread, elke keer als er een thread ontvangen wordt dan reset de snitchTimeout
-snitch :: IORef ClockTime -> Chan Event -> IO ()
-snitch ref chan = forever $ do
-    _ <- readChan chan --hierop zit je het meeste gedeelte van de tijd geblokt
-    now <- getClockTime 
-    writeIORef ref now --update de tijd
-
---
--- getHistory :: IORef (Seq Event) -> ActionM ()
-getHistory = do
+getHistory :: Connection -> ActionM ()
+getHistory conn = do
     setHeader "Content-Type" "text/html"
     html "TODO: history implementeren"
-
---returns het aantal seconden tussen nu en de laatste event die je tegen kwam
--- getSnitch :: IORef ClockTime -> ActionM ()
--- getSnitch ref = do
-    -- now <- liftIO getClockTime
-    -- (TOD last _) <- liftIO $ readIORef ref
-    -- setHeader "Content-Type" "text/html"
-    -- html $ BL.pack . show (now-last) --(aantal seconden tussen toen en nu)
-    -- html "TODO: snitch implementeren"
 
 show404 :: ActionM ()
 show404 = do
