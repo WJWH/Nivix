110e54cbdff97180dd885ebd5e6c7ed90ff7ec46
Wander Hillen <wjw.hillen@gmail.com>
2018-08-07 15:54:08 +0200

    Add extra field to event and fix CORS issues

--- nivix.hs
+++ nivix.hs
@@ -28,19 +28,24 @@ main = scotty 80 $ do
 showDashboard :: ActionM () --ActionM () is de type signature van een route handler, het is een monad waarin je de response kan samenstellen
 showDashboard = do
     setHeader "Content-Type" "text/html" --sets the headers, in dit geval de Content-Type header met value text/html
+    setHeader "Access-Control-Allow-Origin" "*"
     file "homepage.html" --body van de response is een file (scottytest.hs dus)
 
 --ontvang een POST request, als het een geldig event is, schrijf het weg naar de database
 receiveEvent :: ActionM ()
 receiveEvent = do
-    conn <- open dbpath
+    liftIO $ print "Event received"
+    conn <- liftIO $ open dbpath
     bdy <- body
     event <- (return $ decode bdy :: ActionM (Maybe Event))
     case (decode bdy) of
-        Nothing -> return ()
-        Just (Stuw batlevel temp) -> liftIO $ do
+        Nothing -> do
+          liftIO $ print "unrecognized event:"
+          liftIO $ print (show bdy)
+          return ()
+        Just (Stuw batlevel temp sensor) -> liftIO $ do
             now <- getCurrentTime
-            execute conn "INSERT INTO Measurements VALUES (?,?,?)" (now,batlevel,temp)
+            execute conn "INSERT INTO Measurements VALUES (?,?,?,?)" (now,batlevel,temp,sensor)
     setHeader "Content-Type" "text/plain"
     text "NO" --could also be "keepalive", but that's for later
 
@@ -48,26 +53,28 @@ receiveEvent = do
 --de logger op de RPi op een versie van GHC werkt die geen Template Haskell ondersteunt vanwege
 --dat de Pi een ARM processor heeft. TH is nodig om Aeson te draaien. Dit is een goed compromis
 --omdat de JSON encoding van deze ook makkelijk handmatig te genereren is op de Pi.
-data Event = Stuw Double Double deriving (Show,Generic)
+data Event = Stuw Double Double Double deriving (Show,Generic)
 instance ToJSON Event
 instance FromJSON Event
 
 -- Will respond with a JSON blob containing the measurements so far
 getHistory :: ActionM ()
 getHistory = do
-    conn <- open dbpath
+    conn <- liftIO $ open dbpath
     results <- liftIO $ (query_ conn "SELECT * from Measurements" :: IO [EventWithTime])
     setHeader "Content-Type" "text/json"
+    setHeader "Access-Control-Allow-Origin" "*"
     text . TL.fromStrict . decodeLatin1 . BL8.toStrict $ encode results
 
 data EventWithTime = EventWithTime  { time :: UTCTime
                                     , battery :: Double
                                     , temperature :: Double
+                                    , sensorLevel :: Double
                                     } deriving (Show,Eq,Generic)
 instance ToJSON EventWithTime
 instance FromJSON EventWithTime
 instance FromRow EventWithTime where
-      fromRow = EventWithTime <$> field <*> field <*> field
+      fromRow = EventWithTime <$> field <*> field <*> field <*> field
 
 
 show404 :: ActionM ()
